6ed8336427e3184a8c35d7c206af0cae184ac441
Austin Wood <Austin.wood1776@live.com>
2023-12-21 11:50:00 -0800

    Added some basic styles and updated nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,11 +4,14 @@ export default function NavBar() {
 
   return (
     <>
-    <ul class="navbar">
-      <Link to="/Instructions"><li class="nav-item">Instructions</li> </Link>
-      <Link to="/Contact"><li class="nav-item">Contact</li></Link>
-      <Link to="/Play"><li class="nav-item">Play</li></Link>
-    </ul>
+    <nav>
+      <ul class="navbar">
+        <Link to="/"><li class="nav-item">Home</li></Link>
+        <Link to="/Instructions"><li class="nav-item">Instructions</li> </Link>
+        <Link to="/Contact"><li class="nav-item">Contact</li></Link>
+        <Link to="/Play"><li class="nav-item">Play</li></Link>
+      </ul>
+    </nav>
     </>
   )
 };
